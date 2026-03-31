37956cf1030c1f6bb0ad887a9416be31858d1683
nasrinpathan <48160256+nasrinpathan@users.noreply.github.com>
2026-03-31 16:36:59 +0100

    Create config.js via browser

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCpZDkAAEwC8JGGRkSVfg6EX7eY3OE1Dms",
+  authDomain: "thesecretroom-b27e4.firebaseapp.com",
+  databaseURL: "https://thesecretroom-b27e4-default-rtdb.firebaseio.com",
+  projectId: "thesecretroom-b27e4",
+  storageBucket: "thesecretroom-b27e4.firebasestorage.app",
+  messagingSenderId: "963638825603",
+  appId: "1:963638825603:web:516a7f44cb31f9babd41b6",
+  measurementId: "G-6HZY9PK5EW"
+};
